4080593bc1fdb601e49c01c556aa01ff28e1e9ad
unknown <jonathankwon@yahoo.com>
2024-06-11 02:39:30 -0700

    added cone method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
+    public static void cone() {
+        for (int line = 5; line >= 1; line--) {
+            for (int i = 1; i <= (line - 1); i++) {
+                System.out.print(" ");
+            }
+            for (int i = 1; i <= (11 - 2 * line); i++) {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        cone();
+        cone();
     }
 }
